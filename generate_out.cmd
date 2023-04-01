859bd8d1c62caa1ef248905371a840f5512ad58e
jiansyuan <jiansyuan@qq.com>
2023-04-01 20:57:55 +0800

    update sdk to bds1.19.73.01 also LLoader2.12.3

---
+++ generate_out.cmd
@@ -0,0 +1,6 @@
+copy .\StaminaManager\out\build\Release\Release\plugin.dll .\OUT\StaminaManager.dll
+copy .\NBGraphicEngine\out\build\Release\Release\plugin.dll .\OUT\NBGraphicEngine.dll
+copy .\Mt19937RndOre\out\build\Release\Release\plugin.dll .\OUT\Mt19937RndOre.dll
+copy .\JusticeKeeper\out\build\Release\Release\plugin.dll .\OUT\JusticeKeeper.dll
+pause
+
